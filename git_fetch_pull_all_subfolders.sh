8f7e1e464db204b57c989c353431aed75b7d9438
BuildTools <aidencooper23@gmail.com>
2025-05-01 22:27:06 -0400

    Unstable - git_fetch fixed

--- git_fetch_pull_all_subfolders.sh
+++ git_fetch_pull_all_subfolders.sh
@@ -4,7 +4,7 @@
 # Uncomment if you want the script to always use the scripts
 # directory as the folder to look through
 #REPOSITORIES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPOSITORIES=`pwd`
+REPOSITORIES="$HOME/Documents/DARC/BarSync/"
 
 IFS=$'\n'
 
